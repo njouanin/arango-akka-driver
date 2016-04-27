e1feb5b95ffec2111c1adb2bb45199391b3d753e
njouanin <nico@beerfactory.org>
2016-04-27 18:10:56 +0200

    Add host list configuration

--- src/main/scala/io/tripod/arango/ArangoConfigure.scala
+++ src/main/scala/io/tripod/arango/ArangoConfigure.scala
@@ -1,8 +1,12 @@
 package io.tripod.arango
 
+import com.typesafe.config.{Config, ConfigFactory}
+
 /**
   * Created by njouanin on 27/04/16.
   */
-class ArangoConfigure {
+class ArangoConfigure(config: Config=ConfigFactory.load()) {
+  private val hostList = List(config.getList("arangodb.hosts").unwrapped())
+  val nextHost = Iterator.continually(hostList).flatten
 
 }
