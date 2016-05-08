bcff4356b3628de755e17283ee96ea8bb206bf88
Nicolas <nicolas.jouanin@gmail.com>
2016-05-08 21:51:26 +0200

    WIP

--- src/main/scala/io/tripod/arango/ArangoConfigure.scala
+++ src/main/scala/io/tripod/arango/ArangoConfigure.scala
@@ -1,12 +1,39 @@
 package io.tripod.arango
 
+import java.net.URI
+
 import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.collection.JavaConversions._
 
 /**
   * Created by njouanin on 27/04/16.
   */
-class ArangoConfigure(config: Config=ConfigFactory.load()) {
-  private val hostList = List(config.getList("arangodb.hosts").unwrapped())
-  val nextHost = Iterator.continually(hostList).flatten
 
+case class ArangoHost(hostName: String, port: Int)
+
+object ArangoHost {
+  def apply(url: String ) = {
+    val uri = new URI(url)
+    new ArangoHost(uri.getHost, uri.getPort)
+  }
+}
+
+
+class ArangoConfigure(hostList: List[ArangoHost], user: String, password: String, defaultDatabase: String) {
+  //private val hostList = List(config.getList("arangodb.hosts").unwrapped())
+
+  //def nextHost = Iterator.continually(hostList).flatten
+
+}
+
+object ArangoConfigure {
+  def apply(): ArangoConfigure = {
+    val config = ConfigFactory.load()
+    for (host <- config.getStringList("arangodb.hosts")) {
+
+    }
+    val hosts = config.getStringList("arangodb.hosts").map( ArangoHost(_) )
+    new ArangoConfigure(hosts, "","","")
+  }
 }
